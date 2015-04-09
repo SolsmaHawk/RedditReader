0c672d9ffe83d9a19502c9ee3f47b46c162dc256
John Solsma <solsma@me.com>
2015-04-09 16:29:28 -0500

    initial commit

---
+++ CodeSample/GFTableViewCell.h
@@ -0,0 +1,14 @@
+//
+//  GFTableViewCell.h
+//  CodeSample
+//
+//  Copyright (c) 2014 Golden Frog, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GFTableViewCell : UITableViewCell
+
+@property(nonatomic, strong) IBOutlet UILabel *title;
+
+@end
